d430a4da204c306eafb4a17434a7ac076b520133
lemona-97 <wcbe9745@naver.com>
2023-10-23 12:46:52 +0900

    Step1, 2 done

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by wooseob on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
