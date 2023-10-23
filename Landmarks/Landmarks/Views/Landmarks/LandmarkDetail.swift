48b196d29f4aa8687472ebdf8d0b699bb246f32b
lemona-97 <wcbe9745@naver.com>
2023-10-23 14:49:36 +0900

    Section 3 done

--- Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @Environment(ModelData.self) var modelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
@@ -22,8 +29,11 @@ struct LandmarkDetail: View {
             
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 
                 
                 HStack {
@@ -52,5 +62,5 @@ struct LandmarkDetail: View {
 }
 
 #Preview {
-    LandmarkDetail(landmark: landmarks[0])
+    LandmarkDetail(landmark: ModelData().landmarks[0])
 }
---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by wooseob on 10/23/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    var body: some View {
+        NavigationSplitView {
+            List{
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+        .environment(ModelData())
+}
